4997de7d6ded498145f00b56091286af99a0836b
msoderblom <soderblom.matilda@gmail.com>
2019-10-31 15:17:25 +0100

    Added css-file and created a deleteListitem function

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const addToDoForm = document.querySelector('#add_todo')
 const newToDoField = document.querySelector('#new_todo_field')
 const toDoUl = document.querySelector('#todo_list')
 const toDoList = []
+const deleteIcon = '🗑️'
 
 /*****************************/
 
@@ -13,13 +14,22 @@ function addToList() {
     drawList(toDoList)
     
 }
+function deleteListitem(event) {
+    console.log('delete');
+    
+}
 
 function drawList(list) {
     toDoUl.innerHTML = ''
 
     list.forEach(item => {
         const li = document.createElement('li')
-        li.textContent = item
+        const deleteBtn = document.createElement('span')
+        deleteBtn.textContent = deleteIcon
+        deleteBtn.classList.add('delete_btn')
+        deleteBtn.addEventListener('click', deleteListitem)
+        li.textContent = item 
+        li.appendChild(deleteBtn)
         toDoUl.appendChild(li)
     });
 }
