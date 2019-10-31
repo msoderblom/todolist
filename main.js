1ece0b380a2b27550fc6a725a7b45c7d7bae62fa
msoderblom <soderblom.matilda@gmail.com>
2019-10-31 14:53:19 +0100

    Added form-elements and functionality for adding a new todo

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
+
+// Globala variabler
+const addToDoForm = document.querySelector('#add_todo')
 const newToDoField = document.querySelector('#new_todo_field')
+const toDoUl = document.querySelector('#todo_list')
+const toDoList = []
+
+/*****************************/
+
+function addToList() {
+    const newToDo = newToDoField.value
+    toDoList.push(newToDo)
+    drawList(toDoList)
+    
+}
+
+function drawList(list) {
+    toDoUl.innerHTML = ''
+
+    list.forEach(item => {
+        const li = document.createElement('li')
+        li.textContent = item
+        toDoUl.appendChild(li)
+    });
+}
+
+
+addToDoForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    addToList()
+    newToDoField.value = ''
+    
+})
+
 
 
 
