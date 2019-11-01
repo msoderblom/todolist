70e3ea65de89def94ac4662d18d61c810f266418
msoderblom <soderblom.matilda@gmail.com>
2019-11-01 13:08:38 +0100

    Added a date input and set default to today's date with a function

--- main.js
+++ main.js
@@ -2,11 +2,26 @@
 // Globala variabler
 const addToDoForm = document.querySelector('#add_todo')
 const newToDoField = document.querySelector('#new_todo_field')
+const dateField = document.querySelector('#date_field')
 const toDoUl = document.querySelector('#todo_list')
 const toDoList = []
 const deleteIcon = '🗑️'
 
 /*****************************/
+
+function getTodaysDate() {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = addZero(today.getMonth()+1)
+    const date = addZero(today.getDate())
+    return `${year}-${month}-${date}`
+}
+function addZero(num) {
+    if (num < 10) {
+        num = `0${num}`
+    }
+    return num
+}
 
 function addToList() {
     const newToDo = newToDoField.value
@@ -15,22 +30,34 @@ function addToList() {
     
 }
 function deleteListitem(event) {
-    console.log('delete');
+    const listitems = document.querySelectorAll('#todo_list li')
+    console.log(listitems);
+    
+    /*
+    toDoList = toDoList.filter(function (item) {
+        return item !== event.dataset.listitem
+    })
+    */
     
 }
 
 function drawList(list) {
     toDoUl.innerHTML = ''
+    let i = 0
 
     list.forEach(item => {
         const li = document.createElement('li')
+        li.dataset.listitem = i
         const deleteBtn = document.createElement('span')
+        deleteBtn.dataset.listitem = i
         deleteBtn.textContent = deleteIcon
         deleteBtn.classList.add('delete_btn')
         deleteBtn.addEventListener('click', deleteListitem)
         li.textContent = item 
         li.appendChild(deleteBtn)
         toDoUl.appendChild(li)
+
+        i++
     });
 }
 
@@ -42,6 +69,12 @@ addToDoForm.addEventListener('submit', function (event) {
     newToDoField.value = ''
     
 })
+
+
+dateField.value = getTodaysDate()
+
+
+
 
 
 
